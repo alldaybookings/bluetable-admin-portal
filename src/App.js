5a71a44b1465f9992306db556defea082e37f92e
tony-tripulca <tony.tripulca@gmail.com>
2022-11-20 12:13:40 +0800

    Feat: Side navigation

--- src/App.js
+++ src/App.js
@@ -2,24 +2,84 @@ import React, { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./AppRouter";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import { createTheme, ThemeProvider } from "@mui/material";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 import Topnav from "./components/TopNav";
+import Sidenav from "./components/SideNav";
+
+// #fb2f2f
+// #2ffbfb
+// #acfdfd
 
 function App() {
+  const theme = overrideTheme();
+
   const [active, setActive] = useState(true);
 
   return (
     <React.Fragment>
-      <BrowserRouter>
-        <Topnav setActive={setActive}></Topnav>
-        <AppRouter></AppRouter>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Topnav setActive={setActive}></Topnav>
+          <Sidenav active={active}></Sidenav>
+          <AppRouter active={active}></AppRouter>
+        </BrowserRouter>
+      </ThemeProvider>
     </React.Fragment>
   );
+}
+
+function overrideTheme() {
+  return createTheme({
+    palette: {
+      primary: {
+        light: "#f75f5f",
+        main: "#fb2f2f",
+        dark: "#e52b2b",
+        contrastText: "white",
+      },
+      secondary: {
+        light: "#acfdfd",
+        main: "#2ffbfb",
+        dark: "#26bdbd",
+        contrastText: "black",
+      },
+      plain: {
+        light: "white",
+        main: "white",
+        dark: "white",
+        contrastText: "black",
+      },
+    },
+    components: {
+      MuiToolbar: {
+        styleOverrides: {
+          dense: {
+            height: 32,
+            minHeight: 32,
+          },
+        },
+      },
+      MuiList: {
+        styleOverrides: {
+          root: {
+            marginTop: 0,
+            marginBottom: 0,
+            paddingTop: 0,
+            paddingBottom: 0,
+          },
+        },
+      },
+      MuiListItemIcon: {
+        styleOverrides: { root: { minWidth: "36px" } },
+      },
+    },
+  });
 }
 
 export default App;
---
+++ src/components/SideNav.js
@@ -0,0 +1,162 @@
+import { Link } from "react-router-dom";
+import "./../styles/sidenav.scss";
+
+import {
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Avatar,
+  Divider,
+  Typography,
+} from "@mui/material";
+import { deepPurple } from "@mui/material/colors";
+
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import DiningIcon from "@mui/icons-material/Dining";
+import PeopleIcon from "@mui/icons-material/People";
+import TableRestaurantIcon from "@mui/icons-material/TableRestaurant";
+import SettingsIcon from "@mui/icons-material/Settings";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+export default function Sidenav(props) {
+  const mainMenus = [
+    {
+      label: "Dashboard",
+      link: `/dashboard`,
+      icon_component: <DashboardIcon />,
+    },
+    {
+      label: "Customers",
+      link: `/customers`,
+      icon_component: <DiningIcon />,
+    },
+    {
+      label: "Owners",
+      link: `/owners`,
+      icon_component: <PeopleIcon />,
+    },
+    {
+      label: "Restaurants",
+      link: `/restaurants`,
+      icon_component: <TableRestaurantIcon />,
+    },
+  ];
+
+  const settingMenus = [
+    {
+      label: "System",
+      link: `/system`,
+      icon_component: <SettingsIcon />,
+    },
+    {
+      label: "Logout",
+      link: `/logout`,
+      icon_component: <LogoutIcon />,
+    },
+  ];
+
+  return (
+    <Drawer
+      anchor={"left"}
+      variant="persistent"
+      open={props.active}
+      sx={{
+        "& .MuiPaper-root": {
+          top: {
+            xs: 56,
+            sm: 64,
+          },
+        },
+      }}
+    >
+      <Box
+        sx={{
+          width: "100%",
+          minWidth: "12rem",
+          bgcolor: "background.paper",
+        }}
+      >
+        <nav aria-label="main mailbox folders">
+          <Box sx={{ display: "flex", padding: 2 }}>
+            <Avatar sx={{ bgcolor: deepPurple[500] }}>JO</Avatar>
+            <Box sx={{ marginLeft: 1 }}>
+              <Typography sx={{ fontWeight: "bold" }}>John Doe</Typography>
+              <Typography sx={{ fontSize: "0.7rem" }}>Administrator</Typography>
+            </Box>
+          </Box>
+          <Divider />
+          <Typography
+            sx={{
+              margin: 0,
+              paddingTop: 2,
+              paddingLeft: 2,
+              fontSize: "0.6rem",
+              fontWeight: "bold",
+              color: "darkgray",
+            }}
+          >
+            MENU
+          </Typography>
+          <List dense={true}>
+            {mainMenus.map((menu, index) => (
+              <ListItem disablePadding key={index}>
+                <ListItemButton component={Link} to={menu.link}>
+                  <ListItemIcon>{menu.icon_component}</ListItemIcon>
+                  <ListItemText
+                    primary={menu.label}
+                    sx={{
+                      ".MuiTypography-root": {
+                        fontWeight: "bold",
+                        transition: "all 0.2s ease",
+                        ":hover": {
+                          marginLeft: "2px",
+                        },
+                      },
+                    }}
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Typography
+            sx={{
+              margin: 0,
+              paddingTop: 2,
+              paddingLeft: 2,
+              fontSize: "0.6rem",
+              fontWeight: "bold",
+              color: "darkgray",
+            }}
+          >
+            SETTINGS
+          </Typography>
+          <List dense={true}>
+            {settingMenus.map((menu, index) => (
+              <ListItem disablePadding key={index}>
+                <ListItemButton component={Link} to={menu.link}>
+                  <ListItemIcon>{menu.icon_component}</ListItemIcon>
+                  <ListItemText
+                    primary={menu.label}
+                    sx={{
+                      ".MuiTypography-root": {
+                        fontWeight: "bold",
+                        transition: "all 0.2s ease",
+                        ":hover": {
+                          marginLeft: "2px",
+                        },
+                      },
+                    }}
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </nav>
+      </Box>
+    </Drawer>
+  );
+}
